59cce65a23a4ca451285f45b74cd9afc1c30047d
IAML00X <cronoluke@gmail.com>
2017-01-02 10:26:00 +0100

    Classes added

--- src/championship/GUI/controller/MainViewController.java
+++ src/championship/GUI/controller/MainViewController.java
@@ -3,49 +3,25 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package championship.controller;
+package championship.GUI.controller;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
 
 /**
+ * FXML Controller class
  *
  * @author IAMLUX
  */
-public class MainViewController extends Application {
-
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        Scene scene = new Scene(root, 300, 250);
-
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+public class MainViewController implements Initializable {
 
     /**
-     * @param args the command line arguments
+     * Initializes the controller class.
      */
-    public static void main(String[] args) {
-        launch(args);
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
     }
 
 }
